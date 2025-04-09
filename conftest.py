a98ba3ed40a178d1c45e8079b31982b559cc598c
badriaila <ailabadhri@gmail.com>
2025-04-09 00:52:26 -0500

    Updated chrome_options

--- conftest.py
+++ conftest.py
@@ -10,6 +10,12 @@ import os
 def driver(request):
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--window-size=1920,1080")
+    
     driver = webdriver.Chrome(
         service = Service(ChromeDriverManager().install()),
         options = chrome_options
